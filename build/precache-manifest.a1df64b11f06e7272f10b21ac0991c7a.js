fb608502edfbe24d726bdec8f950840cb96238d2
trinhngockhang <20168267@sie.edu.vn>
2019-10-18 13:46:43 +0700

    add file build

---
+++ build/precache-manifest.a1df64b11f06e7272f10b21ac0991c7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c95cb2287a9b730dd3ccd6eb80822b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "063a31046bebc106b75a",
+    "url": "/static/css/2.b15b121b.chunk.css"
+  },
+  {
+    "revision": "af42e3e43e0c03e89164",
+    "url": "/static/css/main.242d5a3b.chunk.css"
+  },
+  {
+    "revision": "063a31046bebc106b75a",
+    "url": "/static/js/2.39f68cd6.chunk.js"
+  },
+  {
+    "revision": "af42e3e43e0c03e89164",
+    "url": "/static/js/main.3060304f.chunk.js"
+  },
+  {
+    "revision": "19eb5804b7e10082c781",
+    "url": "/static/js/runtime-main.2906f212.js"
+  }
+]);
