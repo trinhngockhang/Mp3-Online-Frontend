b730a44bc526f24f2c191796a97c4075029d2bcf
trinhngockhang <20168267@sie.edu.vn>
2019-10-28 11:28:01 +0700

    add search album

--- src/containers/SearchPage.js
+++ src/containers/SearchPage.js
@@ -4,49 +4,44 @@ import { axiosApi } from '../utils/axios';
 import { Row, Col } from 'react-bootstrap';
 import { playSong } from '../actions/action_song';
 import { bindActionCreators } from 'redux';
-
+import { Link } from 'react-router-dom';
 class SearchPage extends Component{
   constructor(props){
     super(props);
     const params = new URLSearchParams(this.props.location.search); 
     const keyword = params.get('keyword');
-    // this.getDataArtist(this.props.match.params.id);
+    this.searchAlbum(keyword);
     this.searchSong(keyword);
     this.state = {};
   }
   componentWillUpdate(nextProps){
     console.log('next', nextProps.location);
     console.log('props', this.props.location);
-    if(nextProps.location.search != this.props.location.search){
+    if(nextProps.location.search !== this.props.location.search){
         const params = new URLSearchParams(nextProps.location.search); 
         const keyword = params.get('keyword');
-        // this.getDataArtist(this.props.match.params.id);
+        this.searchAlbum(keyword);
         this.searchSong(keyword);
     }
   }
-  async getDataArtist(id){
-    const result = await axiosApi.get(`/artist/detail/${id}`);
-    console.log(result);
-    this.setState({...this.state,artist: result.data});
+  async searchAlbum(keyword){
+    const result = await axiosApi.get(`/search/album?_keyword=${keyword}`);
+    this.setState({...this.state, listAlbum: result.data}); 
   }
   async searchSong(keyword){
     const result = await axiosApi.get(`/search/song?_keyword=${keyword}`);
     this.setState({...this.state, listSong: result.data}); 
   }
   render(){
-    if(!this.state.listSong){
-        return (
-          <div >
-  
-          </div>
-        )
-      }
-      console.log(this.state.listSong);
       return (
-        <div className="section">
-          <h3>Kết quả</h3>
-          <Row>
-          {this.state.listSong.map((song, index) => {
+        <div>
+          {
+            this.state.listSong &&
+            (<div className="section">
+              <h3>Bài hát</h3>
+            <Row>
+            {this.state.listSong.map((song, index) => {
+              if(index > 7) return <div></div>
               return(
                 <Col key={index} col lg = {2} md={3} className="playable song" onClick={() =>{
                   this.props.playSong(song);
@@ -64,7 +59,29 @@ class SearchPage extends Component{
               )
             })}
           </Row>
+          </div>)
+          }
+          {/* listAlbum */}
+          {
+            this.state.listAlbum &&
+            (<div className="section">
+              <h3>Album</h3>
+            <Row>
+            {this.state.listAlbum.map((album, index) => {
+            return(
+              <Col key={index} lg = {2} md={2} >
+              <Link to={`/album/${album.id}`}>
+                <img className='song-image' src={album.img} alt={album.albumName}></img>
+                <p>{album.albumName}</p>
+              </Link>
+              </Col>
+            )
+          })}
+          </Row>
+          </div>)
+          }
         </div>
+        
       )
   }
 };
